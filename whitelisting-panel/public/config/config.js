f9c153c19b479f87f0226e1eeafe1b7f88e5dc46
Ben Rauzi <ben.rauzi@outlook.com>
2020-10-27 12:03:23 +1300

    Added Ranks/Departments to Config List

--- whitelisting-panel/public/config/config.js
+++ whitelisting-panel/public/config/config.js
@@ -1,22 +1,65 @@
 var copRanks = {
-    "Recruit": 0,
-    "Patrol": 1,
-
+    "Trainee": 1,
+    "Deputy": 2,
+    "Senior Deputy": 3,
+    "Corporal": 4,
+    "Sergeant": 5,
+    "Lieutenant": 6,
+    "Captain": 7,
+    "Office of the Sheriff": 8,
+    "Sheriff": 9
 };
 
 const copDepartments = {
-
+    "No Department": 0,
+    "DOC": 1,
+    "Patrol": 2,
+    "K9": 3,
+    "GND": 4,
+    "Internal Affairs": 5,
+    "SERT": 6,
+    "SERT Marksman": 7,
+    "OOS": 8,
+    "FBI": 9
 };
 
 const emsRanks = {
-
+    "Volunteer": 1,
+    "Trainee": 2,
+    "Junior EMT": 3,
+    "EMT": 4,
+    "Paramedic": 5,
+    "Senior Paramedic": 6,
+    "Shift Manager": 7,
+    "Lieutenant": 8,
+    "Captain": 9,
+    "OOMC": 10
 };
 
 const emsDepartments = {
-    
+    "No Department": 0,
+    "Fire & Rescue": 1,
+    "Training Academy": 2,
+    "Search & Rescue": 3,
+    "Department Command": 4,
+    "Operations Command": 5,
+    "OOMC": 6
 };
 
 const staffRanks = {
-
+    "Trial Staff": 1,
+    "Moderator": 2,
+    "Administrator": 3,
+    "Senior Administrator": 4,
+    "Head Administrator": 5,
+    "Staff Manager": 6,
+    "Director": 7
 };
 
+const developerRanks = {
+    "Trial Developer": 1,
+    "Junior Developer": 2,
+    "Developer": 3,
+    "Senior Developer": 4,
+    "Lead Developer": 5,
+}
